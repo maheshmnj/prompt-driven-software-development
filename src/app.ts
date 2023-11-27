1a8036d88351512d115e0cbd2127ec3e4248da9e
mahesh jamdade <maheshmn121@gmail.com>
2023-11-27 08:45:22 -0500

    connect to database on server start

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes';
 import authRouter from './routes/authRouter';
+import pool from './db';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRouter);
+
+pool
+    .connect()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((error: any) => {
+        console.error('Unable to connect to the database:', error);
+    });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
